bf2ceef79fd8b3bd301b7b1e69ff3e4e8d35f694
Muhammad Mahmoud Attia <mohamed.attia@careem.com>
2022-11-29 00:58:29 +0200

    add project structure

---
+++ src/main/java/com/attia/vc/VirtualCurrencyApplication.java
@@ -0,0 +1,13 @@
+package com.attia.vc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VirtualCurrencyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VirtualCurrencyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/attia/vc/VirtualCurrencyApplicationTests.java
@@ -0,0 +1,9 @@
+package com.attia.vc;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtualCurrencyApplicationTests {
+
+
+}
